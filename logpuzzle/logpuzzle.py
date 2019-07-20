c79fa3b64b167d68fe8ac9602db5e5e8354f6828
Eliandro <eliandro.ribeiro@gmail.com>
2019-07-19 22:23:26 -0300

    Resolve logpuzzle: download_images

--- logpuzzle/logpuzzle.py
+++ logpuzzle/logpuzzle.py
@@ -9,7 +9,8 @@
 import os
 import re
 import sys
-import urllib
+from urllib.request import urlopen
+from time import time
 
 """Logpuzzle exercise
 Given an apache logfile, find the puzzle urls and download the images.
@@ -25,18 +26,27 @@ def read_urls(filename):
     Screens out duplicate urls and returns the urls sorted into
     increasing order."""
 
+    # Lê o conteúdo do arquivo de log.
     with open(filename, 'r') as arquivo:
         conteudo = arquivo.read()
 
+    # Define o hostname a partir do nome do arquivo.
     hostname = filename.split('_')[-1]
+
+    # A classe set garante que haverá apenas uma única instância de cada URL.
     urls_unicas = set()
+
+    # Localiza o caminho do arquivo de imagem do puzzle dentro do conteúdo do arquivo de registro.
     pattern = re.compile(r'(?:GET )(/[\w\d\./-]+/puzzle/[\w\d\./-]+) ')
 
+    # Monta as URLs para cada imagem encontrada.
     for match in pattern.finditer(conteudo):
-        url = hostname + match[1]
+        url = f'https://{hostname}{match[1]}'
         urls_unicas.add(url)
 
-    urls = sorted(urls_unicas)
+    # Ordena sempre pelo último código de 4 letras na URL.
+    # https://developers.google.com/edu/python/exercises/log-puzzle#part-c---image-slice-descrambling
+    urls = sorted(urls_unicas, key=lambda u: u[-8:-4])
     return urls
 
 
@@ -48,7 +58,52 @@ def download_images(img_urls, dest_dir):
     with an img tag to show each local image file.
     Creates the directory if necessary.
     """
-    # +++your code here+++
+
+    # Cria o diretório destino definido pelo usuário, se não houver.
+    os.makedirs(dest_dir, exist_ok=True)
+
+    # Cria o HTML que reúne os fragmentos da imagem.
+    with open(f'{dest_dir}/index.html', 'wt') as html:
+        html.write(
+'''<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Logpuzzle</title>
+</head>
+<body style="font-size: 0;">
+''')
+        html.flush()
+
+        # Adiciona ao HTML cada um dos fragmentos da imagem.
+        for i, url in enumerate(img_urls):
+            nome_img = f'img{i}.jpg'
+            html.write(f'    <img src="{nome_img}" alt="Fragmento {i}">\n')
+            html.flush()
+
+            caminho_jpg = f'{dest_dir}/{nome_img}'
+
+            try:
+                if not os.path.exists(caminho_jpg): # Apenas para facilitar os testes
+                    inicio = time()
+
+                    # Baixa e grava no diretório o fragmento da imagem.
+                    with urlopen(url, timeout=1.5) as resp:
+                        bytes_img = resp.read()
+                    with open(caminho_jpg, 'wb') as jpg:
+                        jpg.write(bytes_img)
+
+                    fim = time() - inicio
+                    print(f'Download de {url} para {caminho_jpg} em {round(fim, 1)} segundos')
+            except IOError as e:
+                print(e)
+
+        html.write(
+'''</body>
+</html>
+''')
 
 
 def main():
